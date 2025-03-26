da280b088383ae81a3bc2e1f3d4c3db89f2fc3b4
eli1711 <elihofnimariano@gmail.com>
2025-03-25 22:16:01 -0300

    25/03/2025

---
+++ avaliaçãoCPA/avaliaçãoCPA/src/main/java/com/avaliacao/avaliacaoCPA/AvaliacaoCpaApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.avaliacaoCPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoCpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoCpaApplication.class, args);
+	}
+
+}
---
+++ avaliaçãoCPA/avaliaçãoCPA/src/test/java/com/avaliacao/avaliacaoCPA/AvaliacaoCpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.avaliacaoCPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoCpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
